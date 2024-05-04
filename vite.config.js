2c969814e009322513af419d51a386891e74263a
Gadielo03 <142538637+Gadielo03@users.noreply.github.com>
2024-05-04 12:49:33 -0600

    fix: deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://Gadielo03.github.io/Todo-app"
+  base:"https://Gadielo03.github.io/Todo-App"
 })
